0bf45f076e32f23a8b18b4ff7067807663d942dd
Craig Buchanan <craig.buchanan@cogniza.com>
2020-12-23 10:05:04 -0600

    enhancing help adding outputtype adding debug messages

--- PsNLog/Public/New-NLogTarget.ps1
+++ PsNLog/Public/New-NLogTarget.ps1
@@ -1,26 +1,30 @@
 <#
 .SYNOPSIS
-Creates a new logging target
+Creates a new logging target.
 
 .DESCRIPTION
-Logging targets are required to write down the log messages somewhere
+Creates a new logging target.
 
 .PARAMETER TargetType
 
 .EXAMPLE
-$FileLogTarget = New-NLogTarget -TargetType 'file'
+$Target = New-NLogTarget -TargetType FileTarget
 
 .LINK
-https://nlog-project.org/documentation/v3.2.1/html/N_NLog_Targets.htm
+https://nlog-project.org/documentation/v4.7.0/html/N_NLog_Targets.htm
 #>
 function New-NLogTarget() {
 
-	[CmdletBinding()]
+    [CmdletBinding()]
+    [OutputType([NLog.Targets.Target])]
 	param ( 
         [parameter(Mandatory)]
         [string]$TargetType
     )
-	
+    
+    Write-Debug $MyInvocation.MyCommand.Name
+    Write-Debug "TargetType: $TargetType"
+
 	[System.Activator]::CreateInstance( [type]"NLog.Targets.$TargetType" )
 
 }
