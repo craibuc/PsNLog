9ba6f57b52c1ce467997d52fb3fd9f237167dba7
Craig Buchanan <craig.buchanan@cogniza.com>
2020-12-23 09:51:03 -0600

    corrections

--- Examples/MailLog.ps1
+++ Examples/MailLog.ps1
@@ -4,30 +4,30 @@ Import-Module PsNLog -Force
 $MailTarget = New-NLogTarget -TargetType 'MailTarget'
 $MailTarget.Layout = '${logger}|${date:format=yyyy-MM-dd HH\:mm\:ss}|${level:uppercase=true}|${message}'
 
-$MailTarget.SmtpServer	    = "smtp.office365.com"
+$MailTarget.SmtpServer	    = 'smtp.domain.tld'
 $MailTarget.SmtpPort		= "587"
 $MailTarget.EnableSsl		= $true
-$MailTarget.smtpUserName	= ""
-$MailTarget.smtpPassword	= ""
-$MailTarget.From			= ""
-$MailTarget.To			    = ""
+$MailTarget.smtpUserName	= 'USERNAME'
+$MailTarget.smtpPassword	= 'PASSWORD'
+$MailTarget.From			= 'FROM'
+$MailTarget.To			    = 'TO'
 $MailTarget.Encoding 		= [System.Text.Encoding]::GetEncoding("iso-8859-2")
 $MailTarget.Subject		    = "[NLog] MailLogger Test"
 
 # configuration
-$Config = New-NLogConfig
+$Config = New-NLogConfiguration
 $Config.AddTarget("mail", $MailTarget)
 
 $Rule = New-NLogRule -Pattern '*' -LogLevel ([NLog.LogLevel]::Trace) -Target $MailTarget
 $Config.LoggingRules.Add($Rule)
 
 # Create a new Logger
-$Log = New-NLogLogger -Name 'MailLog' -Configuration $Config
+$Logger = New-NLogLogger -Name 'MailLog' -Configuration $Config
 
-# Write test Log messages
-$Log.Debug("Debug Message") # sends the message
-# $Log.Info("Info Message")
-# $Log.Warn("Warn Message")
-# $Log.Error("Error Message")
-# $Log.Trace("Trace Message")
-# $Log.Fatal("Fatal Message")
+# Write test Log messages (one mail message each)
+$Logger.Debug("Debug Message") # sends the message
+$Logger.Info("Info Message")
+$Logger.Warn("Warn Message")
+$Logger.Error("Error Message")
+$Logger.Trace("Trace Message")
+$Logger.Fatal("Fatal Message")
