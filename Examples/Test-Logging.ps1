4605a0acdf5ca64f792311108802c374d292367b
Craig Buchanan <craig.buchanan@cogniza.com>
2021-01-12 18:38:08 -0600

    refactoing

--- Examples/Test-Logging.ps1
+++ Examples/Test-Logging.ps1
@@ -1,23 +1,37 @@
 Import-Module PsNLog -Force
 
-$ScriptName = (Get-Item $PSCommandPath).Basename
-$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+try {
 
-# configuration
-$Configuration = New-NLogConfiguration -Path "$Here/$ScriptName.NLog.config"
-$Configuration.Variables["ScriptDirectory"] = $Here
+    $ScriptName = (Get-Item $PSCommandPath).Basename
+    $Here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-# logger
-$Logger = New-NLogLogger -Configuration $Configuration
+    # configuration
+    $Configuration = New-NLogConfiguration -Path "$Here/$ScriptName.NLog.config"
+    $Configuration.Variables["ScriptDirectory"] = $Here
 
-# messages
-$Logger.Trace("Trace Message")
-$Logger.Debug("Debug Message")
-$Logger.Info("Info Message")
-# structured logging
-$Logger.Info("Created {typeName} #{primary_key_int}", 'INVOICE', 111);
-$Logger.Info("Created {typeName} #{primary_key_varchar}", 'BILL', 'ABC');
-# /structured logging
-$Logger.Warn("Warn Message")
-$Logger.Error( [System.IO.FileNotFoundException]::new('the file wasn''t found') )
-$Logger.Fatal("Fatal Message")
+    # logger
+    $Logger = New-NLogLogger -Configuration $Configuration
+
+    # messages
+    $Logger.Trace("Trace Message")
+    $Logger.Debug("Debug Message")
+    $Logger.Info("Info Message")
+    # structured logging
+    $Logger.Info("Created {typeName} #{primary_key_int}", 'INVOICE', 111);
+    $Logger.Info("Created {typeName} #{primary_key_varchar}", 'BILL', 'ABC');
+    # /structured logging
+    $Logger.Warn("Warn Message")
+
+    $Vendor = 'ABC'
+    $Invoice = 111
+
+    # throw [System.IO.FileNotFoundException]::new('the file wasn''t found')
+    throw "invalid credentials"
+}
+catch [System.IO.FileNotFoundException] {
+    $Message = "Vendor {vendor}/Invoice {invoice}", $Vendor, $Invoice
+    $Logger.Error( $_.Exception , $Message)
+}
+catch {
+    $Logger.Fatal( $_.Exception , "a fatal exception occurred")
+}
