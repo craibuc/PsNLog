64210f7a90ad5b0bd5b7902c3b70961a37ec7a17
Craig Buchanan <craig.buchanan@cogniza.com>
2021-01-12 18:37:24 -0600

    standardizing psakefile

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,27 +1,16 @@
-# Include ./shared.psakefile.ps1
+Include ../lorenz.psakefile.ps1
 
 Properties {
-    $ModuleName='PsNLog'
-    $RepositoryName='Lorenz'
+    $ModuleName='PsIntacctSdk'
   }
   
-FormatTaskName "-------- {0} --------"
-
-Task default -depends List
-
-Task List -description 'List all tasks' {
-    psake -docs
-}
-
-Task Symlink -description "Create a symlink for '$ModuleName' module" {
-    $Here = Get-Location
-    Push-Location ~/.local/share/powershell/Modules
-    ln -s "$Here/$ModuleName" $ModuleName
-    Pop-Location
-}
-  
-Task Publish -description "Publish module '$ModuleName' to repository '$RepositoryName'" {
-
-    Publish-Module -name $ModuleName -Repository $RepositoryName
-
-}
+  Task Symlink -description "Create a symlink for '$ModuleName' module" {
+      $Here = Get-Location
+      Push-Location ~/.local/share/powershell/Modules
+      ln -s "$Here/$ModuleName" $ModuleName
+      Pop-Location
+  }
+    
+  Task Publish -description "Publish module '$ModuleName' to repository '$RepositoryName'" {
+      Publish-Module -name $ModuleName -Repository $RepositoryName
+  }
